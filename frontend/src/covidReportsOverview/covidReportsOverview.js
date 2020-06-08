a8bd24b40dd1bcbcc894c390b263c36374a00c84
Daniel Hermann <dthermann@gmail.com>
2020-06-09 00:22:01 +0200

    add api call to list all states

--- frontend/src/covidReportsOverview/covidReportsOverview.js
+++ frontend/src/covidReportsOverview/covidReportsOverview.js
@@ -7,56 +7,61 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import { useAsyncResource } from 'use-async-resource';
 
 const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
+  table: {},
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+const fetchStates = () => 
+    fetch(`http://localhost:8070/federal-states`)
+        .then(res => res.json())
 
  function ReportsOverview() {
+    const [fetchAllStates] = useAsyncResource(fetchStates, []);  
+
+    return (
+        <React.Suspense fallback="states are loading">
+            <OverviewTable fetchAllStates={fetchAllStates} />
+        </React.Suspense>
+  );
+}
+
+function OverviewTable({ fetchAllStates }) {
     const classes = useStyles();
+    const states = fetchAllStates();
 
     return (
         <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
+            <Table className={classes.table} stickyHeader aria-label="simple table">
                 <TableHead>
-                <TableRow>
-                    <TableCell>Dessert (100g serving)</TableCell>
-                    <TableCell align="right">Calories</TableCell>
-                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                </TableRow>
+                    <TableRow>
+                        <TableCell>Bundesland</TableCell>
+                        <TableCell align="right">gemeldete Fälle</TableCell>
+                        <TableCell align="right">davon genesen</TableCell>
+                        <TableCell align="right">Todesfälle</TableCell>
+                    </TableRow>
                 </TableHead>
                 <TableBody>
-                {rows.map((row) => (
-                    <TableRow key={row.name}>
-                    <TableCell component="th" scope="row">
-                        {row.name}
-                    </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
-                    </TableRow>
-                ))}
+                    {states.map(state => 
+                        <TableRow key={state.id}>
+                            <TableCell>
+                                {state.name}
+                            </TableCell>
+                            <TableCell align="right">
+                                {0}
+                            </TableCell>
+                            <TableCell align="right">
+                                {0}
+                            </TableCell>
+                            <TableCell align="right">
+                                {0}
+                            </TableCell>
+                        </TableRow>)}
                 </TableBody>
             </Table>
         </TableContainer>
-  );
+    );
 }
 
 export { ReportsOverview };
