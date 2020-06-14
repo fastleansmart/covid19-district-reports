aaeb68f67dd0d82db1d84796f67e53c9e22c7899
Daniel Hermann <dthermann@gmail.com>
2020-06-14 22:44:06 +0200

    make notFound component work correctly by utilzing <Switch> component

--- frontend/src/components/contentContainer/contentContainer.js
+++ frontend/src/components/contentContainer/contentContainer.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import { ReportsOverviewContainer } from "../../ReportsOverview/ReportsOverviewContainer";
 import { ReportForm } from "../../reportForm/reportForm";
 import { NotFound } from "../../NotFound/NotFound";
 
 class ContentContainer extends React.Component {
-  render() {
-    return (
-      <div id="content">
-        <Route path="/" exact>
-          <Redirect to="/overview" />
-        </Route>
-        <Route path="/overview" exact component={ReportsOverviewContainer} />
-        <Route path="/add" exact component={ReportForm} />
-        <Route path="/404"  component={NotFound} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div id="content">
+                <Switch>
+                    <Route path="/" exact>
+                        <Redirect to="/overview" />
+                    </Route>
+                    <Route path="/overview" exact component={ReportsOverviewContainer} />
+                    <Route path="/add" exact component={ReportForm} />
+                    <Route component={NotFound} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export { ContentContainer };
